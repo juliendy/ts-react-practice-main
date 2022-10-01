16eec2b284ce6656599d8773eb76e7abc4f46881
juliendy <julien.yasar@gmail.com>
2022-10-01 16:05:13 +0200

    initialising new state

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
   function App() {
+    const [colour, setColour] = useState();
+
+    useEffect(() => {
+      // TODO: generate a random colour
+    })
+    
    return <div className="App">
       <div className='guess-me'></div>
     </div>
